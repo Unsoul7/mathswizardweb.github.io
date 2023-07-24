62d807dab436b8cd91ba7353269659c876f1c606
unsoul7 <disketteit@gmail.com>
2023-07-24 05:41:05 -0700

    Uploaded

---
+++ script.js
@@ -0,0 +1,10 @@
+function themechange() {
+    cl1 = document.getElementById("header")
+    cl1.classList.toggle("theme-dark")
+
+    cl2 = document.getElementById("nav")
+    cl2.classList.toggle("theme-dark")
+
+    cl3 = document.getElementById("list")
+    cl3.classList.toggle("text-dark")
+}
